3242b94d4de33d50196ce22ab02be5a60ac0db37
Gustavo Ragghiante <100872115+Ragghi@users.noreply.github.com>
2025-08-08 19:11:18 +0000

    exercicio 1 declaracao de variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,17 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Gustavo Ragghi'
+empresa = "Stefanini"
+
 
 # Declare variáveis de cada tipo numérico
-
+idade = 38
+filhos = 1
+dogs = 2
+altura = 1.80
+peso = 80.00
 
 # Declare uma variável do tipo booleano
+ScrumMaster = True
+maior_idade = idade >= 18
