4dd36673acce231ccb7ace802c83a53418289712
Gustavo Ragghiante <100872115+Ragghi@users.noreply.github.com>
2025-08-11 19:39:12 +0000

    exercicios numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,23 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+nome = "Gustavo"
+print(nome * 4)
 
 # Realize uma operação entre dados do tipo inteiro e float
+idade = 38
+print(idade + 8.9)
+
+#Arredondamento
+arred = 19/3
+print(arred)
+print(round(arred,0))
+print(round(arred,1))
+
+#Operações utilizando o String Format
+print(f'Gustavo estudou hoje {3 + 1} horas')
