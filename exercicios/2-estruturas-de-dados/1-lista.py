529958ab33f76c2865c7768b7da9580502f5d24a
Gustavo Ragghiante <100872115+Ragghi@users.noreply.github.com>
2025-08-19 18:13:15 +0000

    exercicio de lista

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -3,14 +3,14 @@ lista_numerica = [2, 4, 6, 8, 10, 12]
 print(lista_numerica)
 
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-lista = [1, 3, "Gustavo", 8, {8 > 4}, {3 < 9 and 9 < 11}, {8 + 4}]
+lista = [1, 3, "Gustavo", 8, {8 > 4}, [1990, 2010, 2025], {3 < 9 and 9 < 11}, {8 + 4}]
 print(lista)
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
 print(lista[0:5])
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-print(lista[0:-1:2])
+print(lista[::2])
 
 # Remova da lista o último item
 print(lista.pop())
