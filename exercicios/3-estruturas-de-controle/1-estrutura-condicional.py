fb6a02a672a1ca4d8d0a99d3c6dceefaedbbd1d5
Gustavo Ragghiante <100872115+Ragghi@users.noreply.github.com>
2025-08-25 00:22:10 +0000

    exercicio condicional

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,14 +1,26 @@
 # Declare 4 variáveis do tipo numérica
-
+x = 8
+p = 10
+t = 5
+o = 2
 
 # Crie uma estrutura condicional para comparar dois números
-
+if x > o:
+  print(True)
+else:
+  print(False)
 
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
-
+if x > o:
+  print(f"Neste caso {x} é maior que {o}")
+else:
+  print(f"Aqui {x} é menor que {o}")
 
 # Se a condição não for cumprida, imprima na tela uma mensagem informando que a condição é negativa e informe o número de maior valor
-
+if t > p:
+  print(f"Neste caso {t} é maior que {p}")
+else:
+  print(f"Aqui {t} é menor que {p}")
 
 # Insira outras condições na estrutura condicional usando o elif
 
